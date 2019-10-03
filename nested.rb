2fa47c677b77f3cc72860076a5189efee6f0bc28
Eli Kim <eliskim27@gmail.com>
2019-10-03 12:56:46 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -132,6 +132,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-    programmer_hash[dennis_ritchie][languages].push 
+    programmer_hash[dennis_ritchie][languages].push ("Assembly")
+programmer_hash
 
 end
